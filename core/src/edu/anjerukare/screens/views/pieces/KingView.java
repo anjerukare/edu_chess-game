88ef7a6d55a638d04a547900f13fd92335e9c943
anjerukare <magicskilltv@gmail.com>
2021-05-01 21:00:53 +0700

    Add checked effect to texture atlas and check state in KingView class

--- core/src/edu/anjerukare/screens/views/pieces/KingView.java
+++ core/src/edu/anjerukare/screens/views/pieces/KingView.java
@@ -1,5 +1,7 @@
 package edu.anjerukare.screens.views.pieces;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import edu.anjerukare.Assets;
 import edu.anjerukare.screens.enums.Team;
 import edu.anjerukare.screens.views.PieceView;
@@ -9,11 +11,22 @@ import static edu.anjerukare.screens.enums.PieceType.KING;
 
 public class KingView extends PieceView {
 
+    private TextureRegion checkEffect;
+    public boolean checked;
+
     public KingView(Team team) {
         super(team);
         type = KING;
         textureRegion = Assets.get(piecesAtlas).findRegion(
                 (team == Team.WHITE? "white": "black") + "_king");
         setSize(textureRegion.getRegionWidth(), textureRegion.getRegionHeight());
+        checkEffect = Assets.get(piecesAtlas).findRegion("king_checked");
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        if (checked)
+            batch.draw(checkEffect, getX(), getY());
     }
 }
