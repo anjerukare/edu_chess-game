3fd5649168bbbbdbe356ef96f044201e02e0decb
anjerukare <magicskilltv@gmail.com>
2021-05-02 21:56:15 +0700

    Add checked state to King.java and equals method to Point.java

--- core/src/edu/anjerukare/screens/utils/Point.java
+++ core/src/edu/anjerukare/screens/utils/Point.java
@@ -12,4 +12,12 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
 }
